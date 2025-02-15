7bb09ed65ee6f1e5b592c2400ebebb7228f8c3c9
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 05:50:17 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+void ConvertirYRedondear(string numeroStr)
+{
+    double numero = double.Parse(numeroStr); // Convertir string a double
+    int redondeado = (int)Math.Round(numero); // Redondear al entero más cercano
+
+    Console.WriteLine("Número redondeado: " + redondeado);
+}
+
+ConvertirYRedondear("12.5");
